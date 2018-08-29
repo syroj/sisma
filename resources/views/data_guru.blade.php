4c29d5db0a6655539606bc6e8e1f80379a5d4810
syroj <dev.syroj@gmail.com>
2018-08-29 09:51:38 +0700

    commit

--- resources/views/data_guru.blade.php
+++ resources/views/data_guru.blade.php
@@ -4,10 +4,10 @@
 <div class="main">
     <div class="card">
         <div class="card-header">
-            <h5>halaman data guru</h5>
+            <h5><span><i class="fa fa-users"></i></span> halaman data guru</h5>
         </div>
         <div class="card-body">
-        ini halaman data guru
+        ini halaman data guru <span><i class="fa fa users"></i></span>
         </div>
     </div>
 </div>
